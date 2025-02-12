a3751a21a2284cf2e3af3199bdadaea0b3370750
Udoaniekan <udoaniekan1995@gmail.com>
2025-02-12 12:46:18 +0100

    todo-ms

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true, // add this line to transform the request body into class objects
+  }));
+  app.use(cookieParser());
+  app.setGlobalPrefix('api/v1');
+  const port = process.env.PROJECT_PORT
+  await app.listen(port, ()=> {console.log(`listening on port:${port}`);
+});
+
 }
 bootstrap();
