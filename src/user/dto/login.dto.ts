d1a931864399a16b6d9c2e936deaabd384896762
Udoaniekan <udoaniekan1995@gmail.com>
2025-02-12 12:23:38 +0100

    fixes

--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateUserDto } from "./create-user.dto";
+import { IsNotEmpty, IsString } from "class-validator";
 
-export class LoginDto extends PartialType(CreateUserDto) {}
+export class LoginDto{
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+      
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+}
