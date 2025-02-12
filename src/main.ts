9b3b0e58b8c1d276fa14048e0353f58e2840d92c
Udoaniekan <udoaniekan1995@gmail.com>
2025-02-12 13:36:57 +0100

    filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from 'cookie-parser';
+import { GlobalExceptionFilter } from "./exception/filter.exception";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     transform: true, // add this line to transform the request body into class objects
   }));
   app.use(cookieParser());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.setGlobalPrefix('api/v1');
   const port = process.env.PROJECT_PORT
   await app.listen(port, ()=> {console.log(`listening on port:${port}`);
